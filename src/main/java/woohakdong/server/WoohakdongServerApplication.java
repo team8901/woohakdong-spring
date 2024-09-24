e3e08b9e05cdfbbb4153ef5ab91c8f5db3f03488
sangjun <sangjun1389@ajou.ac.kr>
2024-09-24 16:28:10 +0900

    [WHD-19] Init: Initialize Server Project

---
+++ src/main/java/woohakdong/server/WoohakdongServerApplication.java
@@ -0,0 +1,13 @@
+package woohakdong.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoohakdongServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoohakdongServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/woohakdong/server/WoohakdongServerApplicationTests.java
@@ -0,0 +1,13 @@
+package woohakdong.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoohakdongServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
