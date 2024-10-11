d31ab3ec2b846303a22b57b6da4dc28aafaac792
sangjun <sangjun1389@ajou.ac.kr>
2024-10-11 14:52:11 +0900

    [WHD-87] Feat: Make ClubMember entity
    - #in-progress

---
+++ src/main/java/woohakdong/server/domain/clubmember/ClubMemberRepository.java
@@ -0,0 +1,10 @@
+package woohakdong.server.domain.clubmember;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import woohakdong.server.domain.club.Club;
+import woohakdong.server.domain.member.Member;
+
+public interface ClubMemberRepository extends JpaRepository<ClubMember, Long> {
+
+    Boolean existsByClubAndMemberAndClubMemberRole(Club club, Member member, ClubMemberRole clubMemberRole);
+}
