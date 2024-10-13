915e51b36f6d103d03a6b592f19a1673795cde6f
sangjun <sangjun1389@ajou.ac.kr>
2024-10-14 00:54:36 +0900

    [WHD-99] Feat: Add clubGeneration column to Club entity

--- src/main/java/woohakdong/server/api/controller/club/dto/ClubInfoResponse.java
+++ src/main/java/woohakdong/server/api/controller/club/dto/ClubInfoResponse.java
@@ -1,12 +1,27 @@
 package woohakdong.server.api.controller.club.dto;
 
+import lombok.Builder;
+import woohakdong.server.domain.club.Club;
+
+@Builder
 public record ClubInfoResponse(
         Long clubId,
         String clubName,
-        String clubNameEnglish,
+        String clubEnglishName,
         String clubImage,
         String clubDescription,
         String clubRoom,
         String clubGeneration
 ) {
+    public static ClubInfoResponse from(Club club) {
+        return ClubInfoResponse.builder()
+                .clubId(club.getClubId())
+                .clubName(club.getClubName())
+                .clubEnglishName(club.getClubEnglishName())
+                .clubImage(club.getClubImage())
+                .clubDescription(club.getClubDescription())
+                .clubRoom(club.getClubRoom())
+                .clubGeneration(club.getClubGeneration())
+                .build();
+    }
 }
