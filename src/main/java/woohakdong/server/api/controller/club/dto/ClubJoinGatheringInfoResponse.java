053faa6dae7dc85dc4f7f605c5cf3a141cc177e6
sangjun <sangjun1389@ajou.ac.kr>
2024-10-14 22:18:55 +0900

    [WHD-100] Feat: Make join type gathering get api

--- src/main/java/woohakdong/server/api/controller/club/dto/ClubJoinGatheringInfoResponse.java
+++ src/main/java/woohakdong/server/api/controller/club/dto/ClubJoinGatheringInfoResponse.java
@@ -1,13 +1,24 @@
 package woohakdong.server.api.controller.club.dto;
 
 import lombok.Builder;
+import woohakdong.server.domain.gathering.Gathering;
 
 @Builder
 public record ClubJoinGatheringInfoResponse(
-        int gatheringId,
+        Long gatheringId,
         String gatheringName,
         String gatheringLink,
         String gatheringDescription,
-        int gatheringAmount
+        Integer gatheringAmount
 ) {
+
+    public static ClubJoinGatheringInfoResponse from(Gathering gathering) {
+        return ClubJoinGatheringInfoResponse.builder()
+                .gatheringId(gathering.getGatheringId())
+                .gatheringName(gathering.getGatheringName())
+                .gatheringLink(gathering.getGatheringLink())
+                .gatheringDescription(gathering.getGatheringDescription())
+                .gatheringAmount(gathering.getGatheringAmount())
+                .build();
+    }
 }
