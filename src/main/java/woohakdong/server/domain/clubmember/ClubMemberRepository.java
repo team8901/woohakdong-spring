55746e4dd108a31228db6f6b9f7f6617f121a3ef
juy4844 <uiyeop@ajou.ac.kr>
2024-10-16 02:22:58 +0900

    [WHD-118] Feat: Add club member list api and test

--- src/main/java/woohakdong/server/domain/clubmember/ClubMemberRepository.java
+++ src/main/java/woohakdong/server/domain/clubmember/ClubMemberRepository.java
@@ -1,10 +1,19 @@
 package woohakdong.server.domain.clubmember;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import woohakdong.server.domain.club.Club;
 import woohakdong.server.domain.member.Member;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 public interface ClubMemberRepository extends JpaRepository<ClubMember, Long> {
 
     Boolean existsByClubAndMemberAndClubMemberRole(Club club, Member member, ClubMemberRole clubMemberRole);
+
+    @Query("SELECT cm FROM ClubMember cm WHERE cm.club.clubId = :clubId AND cm.clubMemberAssignedTerm = :assignedTerm")
+    Optional<List<ClubMember>> findByClubIdAndAssignedTerm(@Param("clubId") Long clubId, @Param("assignedTerm") LocalDate assignedTerm);
 }
