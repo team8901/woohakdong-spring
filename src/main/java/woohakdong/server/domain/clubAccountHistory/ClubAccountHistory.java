4ab63b746cd845bb8ed24067785ba8efd11a01aa
juy4844 <uiyeop@ajou.ac.kr>
2024-10-16 23:14:18 +0900

    [WHD-124] Feat: Add ClubAccountHistory domain

---
+++ src/main/java/woohakdong/server/domain/clubAccountHistory/ClubAccountHistory.java
@@ -0,0 +1,50 @@
+package woohakdong.server.domain.clubAccountHistory;
+
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import woohakdong.server.domain.clubAccount.ClubAccount;
+
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class ClubAccountHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long clubAccountHistoryId;
+
+    @Column(nullable = false)
+    private String clubAccountHistoryInOutType;
+
+    @Column(nullable = false)
+    private LocalDate clubAccountHistoryTranDate;
+
+    @Column(nullable = false)
+    private Long clubAccountHistoryBalanceAmt;
+
+    @Column(nullable = false)
+    private Long clubAccountHistoryTranAmt;
+
+    @Column(nullable = false)
+    private String clubAccountHistoryContent;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "club_account_id")
+    private ClubAccount clubAccount;
+
+    @Builder
+    public ClubAccountHistory(String clubAccountHistoryInOutType, LocalDate clubAccountHistoryTranDate,
+                              Long clubAccountHistoryBalanceAmt, Long clubAccountHistoryTranAmt,
+                              String clubAccountHistoryContent) {
+        this.clubAccountHistoryInOutType = clubAccountHistoryInOutType;
+        this.clubAccountHistoryTranDate = clubAccountHistoryTranDate;
+        this.clubAccountHistoryBalanceAmt = clubAccountHistoryBalanceAmt;
+        this.clubAccountHistoryTranAmt = clubAccountHistoryTranAmt;
+        this.clubAccountHistoryContent = clubAccountHistoryContent;
+    }
+}
---
+++ src/main/java/woohakdong/server/domain/clubAccountHistory/ClubAccountHistoryRepository.java
@@ -0,0 +1,6 @@
+package woohakdong.server.domain.clubAccountHistory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClubAccountHistoryRepository extends JpaRepository<ClubAccountHistory, Long> {
+}
