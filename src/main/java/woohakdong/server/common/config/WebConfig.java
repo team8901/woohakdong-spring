2ee5c1d0f2f2d885b02964903e4a2c70f9864e7e
sangjun <sangjun1389@ajou.ac.kr>
2024-10-17 18:44:48 +0900

    [WHD-110] Fix: Change cors settings for web

--- src/main/java/woohakdong/server/common/config/WebConfig.java
+++ src/main/java/woohakdong/server/common/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정
                 .allowedOrigins("http://d1r973zpg8hnu4.cloudfront.net", "http://localhost:3000", "http://localhost") // 허용할 도메인
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+                .allowedMethods("*") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 인증 관련 정보 허용 (쿠키 등)
     }
