7d84e15b512a6d514c439fb46d8414016d2e14a6
juy4844 <uiyeop@ajou.ac.kr>
2024-10-19 02:22:04 +0900

    [WHD-158] Feat: Add Item, ItemHistory domain

---
+++ src/main/java/woohakdong/server/domain/ItemHistory/ItemHistoryRepository.java
@@ -0,0 +1,6 @@
+package woohakdong.server.domain.ItemHistory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemHistoryRepository extends JpaRepository<ItemHistory, Long> {
+}
