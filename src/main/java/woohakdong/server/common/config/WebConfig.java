60e9b805f7bb7f0721c1fe14de861956f04beb2e
sangjun <sangjun1389@ajou.ac.kr>
2024-10-18 14:36:04 +0900

    [WHD-110] Feat: Implement API for portone Webhook

--- src/main/java/woohakdong/server/common/config/WebConfig.java
+++ src/main/java/woohakdong/server/common/config/WebConfig.java
@@ -9,9 +9,20 @@ public class WebConfig implements WebMvcConfigurer {
 
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정
-                .allowedOrigins("http://d1r973zpg8hnu4.cloudfront.net", "http://localhost:3000", "http://localhost") // 허용할 도메인
+                .allowedOrigins(getAllowedOrigins())
                 .allowedMethods("*") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 인증 관련 정보 허용 (쿠키 등)
+    }
+
+    private String[] getAllowedOrigins() {
+        return new String[]{
+                "http://d1r973zpg8hnu4.cloudfront.net",
+                "http://localhost:3000",
+                "http://localhost",
+                "http://52.78.100.19",
+                "http://52.78.48.223",
+                "http://52.78.5.241"
+        };
     }
 }
