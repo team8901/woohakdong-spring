614b48ebaff4ee35c700f7db07d5c73cfd3d2b8f
juy4844 <uiyeop@ajou.ac.kr>
2024-10-20 01:29:49 +0900

    [WHD-158] Feat: Add get item list

--- src/main/java/woohakdong/server/api/controller/item/ItemController.java
+++ src/main/java/woohakdong/server/api/controller/item/ItemController.java
@@ -2,9 +2,13 @@ package woohakdong.server.api.controller.item;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import woohakdong.server.api.controller.ListWrapperResponse;
+import woohakdong.server.api.controller.item.dto.ItemListResponse;
 import woohakdong.server.api.controller.item.dto.ItemRegisterRequest;
 import woohakdong.server.api.controller.item.dto.ItemRegisterResponse;
 import woohakdong.server.api.service.item.ItemService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/clubs")
@@ -16,5 +20,11 @@ public class ItemController {
     @PostMapping("/{clubId}/items")
     public ItemRegisterResponse registerItem(@PathVariable Long clubId, ItemRegisterRequest request) {
         return itemService.registerItem(clubId, request);
+    }
+
+    @GetMapping("/{clubId}/items")
+    public ListWrapperResponse<ItemListResponse> getItems(@PathVariable Long clubId) {
+        List<ItemListResponse> items = itemService.getItemsByClubId(clubId);
+        return ListWrapperResponse.of(items);
     }
 }
