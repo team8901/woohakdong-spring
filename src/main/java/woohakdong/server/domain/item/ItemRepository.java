5c7746b4a0cb8f384cee472ee9efa4d20165227a
juy4844 <uiyeop@ajou.ac.kr>
2024-10-20 01:29:49 +0900

    [WHD-158] Feat: Add get item list

--- src/main/java/woohakdong/server/domain/item/ItemRepository.java
+++ src/main/java/woohakdong/server/domain/item/ItemRepository.java
@@ -2,5 +2,8 @@ package woohakdong.server.domain.item;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    List<Item> findByClubClubId(Long clubId);
 }
