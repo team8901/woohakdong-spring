24ce05f939c9b9068f700c8877cb574fb6916bb5
juy4844 <uiyeop@ajou.ac.kr>
2024-10-21 00:24:25 +0900

    [WHD-167] Feat: rental item for update

--- src/main/java/woohakdong/server/domain/item/ItemRepository.java
+++ src/main/java/woohakdong/server/domain/item/ItemRepository.java
@@ -1,9 +1,18 @@
 package woohakdong.server.domain.item;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByClubClubId(Long clubId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT i FROM Item i WHERE i.itemId = :itemId")
+    Optional<Item> findByIdForUpdate(@Param("itemId") Long itemId);
 }
