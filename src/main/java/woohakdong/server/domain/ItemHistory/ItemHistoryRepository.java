6ff08775a24f72288484439ea8de4066bcd1c30c
juy4844 <uiyeop@ajou.ac.kr>
2024-10-20 23:55:17 +0900

    [WHD-167] Feat: Add item return api

--- src/main/java/woohakdong/server/domain/ItemHistory/ItemHistoryRepository.java
+++ src/main/java/woohakdong/server/domain/ItemHistory/ItemHistoryRepository.java
@@ -1,6 +1,14 @@
 package woohakdong.server.domain.ItemHistory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import woohakdong.server.domain.item.Item;
+import woohakdong.server.domain.member.Member;
+
+import java.util.Optional;
 
 public interface ItemHistoryRepository extends JpaRepository<ItemHistory, Long> {
+    @Query("SELECT ih FROM ItemHistory ih WHERE ih.item = :item AND ih.member = :member AND ih.itemReturnDate IS NULL")
+    Optional<ItemHistory> findActiveBorrowingRecord(@Param("item") Item item, @Param("member") Member member);
 }
