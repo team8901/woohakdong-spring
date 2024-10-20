12a4d157d8b4cb818cbe67767b92d3f427facdf6
juy4844 <uiyeop@ajou.ac.kr>
2024-10-20 12:31:41 +0900

    [WHD-158] Fit: Add RequestBody

--- src/main/java/woohakdong/server/api/controller/item/ItemControllerDocs.java
+++ src/main/java/woohakdong/server/api/controller/item/ItemControllerDocs.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import woohakdong.server.api.controller.ListWrapperResponse;
 import woohakdong.server.api.controller.item.dto.ItemListResponse;
 import woohakdong.server.api.controller.item.dto.ItemRegisterRequest;
@@ -16,7 +17,7 @@ public interface ItemControllerDocs {
     @SecurityRequirement(name = "accessToken")
     @Operation(summary = "물품 등록", description = "동아리 임원진 이상이 물품을 등록하면 물품 Id를 반환한다.")
     @ApiResponse(responseCode = "200", description = "물품 등록 성공", useReturnTypeSchema = true)
-    public ItemRegisterResponse registerItem(@PathVariable Long clubId, ItemRegisterRequest request);
+    public ItemRegisterResponse registerItem(@PathVariable Long clubId, @RequestBody ItemRegisterRequest request);
 
     @SecurityRequirement(name = "accessToken")
     @Operation(summary = "물품 리스트 반환", description = "물품 리스트를 반환한다.")
