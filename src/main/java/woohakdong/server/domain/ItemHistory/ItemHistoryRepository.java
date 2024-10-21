f0696b63e370953d96bce5670bfd99a6470dad57
juy4844 <uiyeop@ajou.ac.kr>
2024-10-21 14:14:07 +0900

    [WHD-167] Feat: Add get item history api and test

--- src/main/java/woohakdong/server/domain/ItemHistory/ItemHistoryRepository.java
+++ src/main/java/woohakdong/server/domain/ItemHistory/ItemHistoryRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.repository.query.Param;
 import woohakdong.server.domain.item.Item;
 import woohakdong.server.domain.member.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ItemHistoryRepository extends JpaRepository<ItemHistory, Long> {
     @Query("SELECT ih FROM ItemHistory ih WHERE ih.item.itemId = :itemId AND ih.member.memberId = :memberId AND ih.itemReturnDate IS NULL")
     Optional<ItemHistory> findActiveBorrowingRecord(@Param("itemId") Long itemId, @Param("memberId") Long memberId);
+
+    @Query("SELECT ih FROM ItemHistory ih WHERE ih.item.itemId = :itemId AND ih.item.club.clubId = :clubId")
+    List<ItemHistory> findByItemAndClub(@Param("itemId") Long itemId, @Param("clubId") Long clubId);
 }
