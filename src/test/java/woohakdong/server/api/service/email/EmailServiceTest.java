877788a432749f8823573c8dac17932f773c54dc
sangjun <sangjun1389@ajou.ac.kr>
2024-10-24 08:58:50 +0900

    [WHD-153] Feat: Implement email logic
    - 그룹 가입 시, 이메일 전송 로직 추가
    - 이메일 발송 실패 시, 총 3회까지 반복
    - 이메일 발송 시, 비동기로 진행
    - 태스트 코드 작성

---
+++ src/test/java/woohakdong/server/api/service/email/EmailServiceTest.java
@@ -0,0 +1,120 @@
+package woohakdong.server.api.service.email;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.willThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static woohakdong.server.common.exception.CustomErrorInfo.MAIL_SEND_ERROR;
+import static woohakdong.server.domain.group.GroupType.JOIN;
+
+import java.util.concurrent.ExecutionException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.SyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+import woohakdong.server.common.exception.CustomException;
+import woohakdong.server.domain.club.Club;
+import woohakdong.server.domain.group.Group;
+import woohakdong.server.domain.member.Member;
+import woohakdong.server.domain.school.School;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+class EmailServiceTest {
+
+    @Autowired
+    private EmailService emailService;
+
+    @MockBean
+    private EmailClientImpl emailClientImpl;
+
+    @TestConfiguration // 테스트에서만 동기적으로 동작하도록 설정
+    static class TestConfig {
+
+        @Bean
+        public TaskExecutor taskExecutor() {
+            return new SyncTaskExecutor();
+        }
+    }
+
+    @DisplayName("동아리에 새로 가입한 사람에게 이메일을 보낼 수 있다.")
+    @Test
+    void sendEmailForGroupJoin() {
+        // Given
+        School school = createSchool();
+        Club club = createClub(school, "테스트 동아리", "testClub");
+        Group group = createGroup(club, "https://test.com", "test1234");
+        Member member = createMember(school, "김우학", "test1234@ajou.ac.kr");
+
+        // When & Then
+        emailService.sendEmailForGroupJoin(member.getMemberName(), member.getMemberEmail(), club.getClubName(),
+                group.getGroupChatLink(), group.getGroupChatPassword());
+    }
+
+    @DisplayName("메일 전송이 실패하면, 최대 3번까지 재시도한다.")
+    @Test
+    void sendEmailForGroupJoinRetryTest() throws InterruptedException, ExecutionException {
+        // Given
+        School school = createSchool();
+        Club club = createClub(school, "테스트 동아리", "testClub");
+        Group group = createGroup(club, "https://test.com", "test1234");
+        Member member = createMember(school, "김우학", "test1234@ajou.ac.kr");
+
+        willThrow(new CustomException(MAIL_SEND_ERROR))
+                .given(emailClientImpl)
+                .sendEmailForGroupJoin(anyString(), anyString(), anyString(), anyString(), anyString());
+
+        // When
+        emailService.sendEmailForGroupJoin(member.getMemberName(), member.getMemberEmail(), club.getClubName(),
+                group.getGroupChatLink(), group.getGroupChatPassword());
+
+        // Then
+        verify(emailClientImpl, times(3))
+                .sendEmailForGroupJoin(anyString(), anyString(), anyString(), anyString(), anyString());
+    }
+
+    private static Group createGroup(Club club, String chatLink, String chatPassword) {
+        return Group.builder()
+                .club(club)
+                .groupJoinLink("https://woohakdong.com/" + club.getClubEnglishName())
+                .groupChatLink(chatLink)
+                .groupChatPassword(chatPassword)
+                .groupType(JOIN)
+                .build();
+    }
+
+    private static Club createClub(School school, String clubName, String club) {
+        return Club.builder()
+                .clubName(clubName)
+                .clubEnglishName(club)
+                .school(school)
+                .build();
+    }
+
+    private static Member createMember(School school, String name, String email) {
+        return Member.builder()
+                .memberName(name)
+                .memberEmail(email)
+                .memberRole("ROLE_USER")
+                .memberProvideId("1234567890")
+                .school(school)
+                .build();
+    }
+
+    private static School createSchool() {
+        return School.builder()
+                .schoolName("아주대학교")
+                .schoolDomain("ajou.ac.kr")
+                .build();
+    }
+
+}
