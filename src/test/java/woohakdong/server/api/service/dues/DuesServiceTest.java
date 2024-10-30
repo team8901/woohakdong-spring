20dbef770c1c0295b35250a36e238f31ef2fd4fa
juy4844 <uiyeop@ajou.ac.kr>
2024-10-30 21:36:29 +0900

    [WHD-145] Feat: Add club account history test

---
+++ src/test/java/woohakdong/server/api/service/dues/DuesServiceTest.java
@@ -0,0 +1,50 @@
+package woohakdong.server.api.service.dues;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+import woohakdong.server.domain.admin.adminAccount.AccountType;
+import woohakdong.server.domain.clubAccount.ClubAccount;
+import woohakdong.server.domain.clubAccount.ClubAccountRepository;
+import woohakdong.server.domain.clubAccountHistory.ClubAccountHistory;
+import woohakdong.server.domain.clubAccountHistory.ClubAccountHistoryRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@Transactional
+class DuesServiceTest {
+    @Autowired
+    private ClubAccountHistoryRepository clubAccountHistoryRepository;
+
+    @Autowired
+    private ClubAccountRepository clubAccountRepository;
+
+    @Test
+    @DisplayName("특정 클럽의 월별 거래 내역을 조회할 수 있다")
+    void findMonthlyTransactions() {
+        // given
+        ClubAccount clubAccount = clubAccountRepository.save(new ClubAccount(null, "Test Bank", "1234567890", "0011223344", "011", LocalDateTime.now()));
+
+        clubAccountHistoryRepository.save(new ClubAccountHistory(AccountType.DEPOSIT, LocalDateTime.of(2024, 10, 15, 10, 0), 100000L, 10000L, "Test Deposit 1", clubAccount));
+        clubAccountHistoryRepository.save(new ClubAccountHistory(AccountType.WITHDRAW, LocalDateTime.of(2024, 10, 20, 15, 30), 90000L, 10000L, "Test Withdraw", clubAccount));
+        clubAccountHistoryRepository.save(new ClubAccountHistory(AccountType.DEPOSIT, LocalDateTime.of(2024, 11, 5, 11, 0), 100000L, 10000L, "Test Deposit 2", clubAccount));
+
+        int year = 2024;
+        int month = 10;
+
+        // when
+        List<ClubAccountHistory> histories = clubAccountHistoryRepository.findMonthlyTransactions(clubAccount, year, month);
+
+        // then
+        assertThat(histories).hasSize(2);
+        assertThat(histories).extracting("clubAccountHistoryContent").containsExactlyInAnyOrder("Test Deposit 1", "Test Withdraw");
+    }
+}
