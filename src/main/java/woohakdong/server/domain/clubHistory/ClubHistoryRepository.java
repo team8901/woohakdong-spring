21536b559ec35a702194ab1bd4920d99a4689e8a
Sangjun Park <sangjun1389@ajou.ac.kr>
2024-11-01 00:37:04 +0900

    Revert "Release Sprint 2 (#21)"
    This reverts commit 2bb8621513629340aa33affaf98e477de29529a6.

--- src/main/java/woohakdong/server/domain/clubHistory/ClubHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package woohakdong.server.domain.clubHistory;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface ClubHistoryRepository extends JpaRepository<ClubHistory, Long> {
-    List<ClubHistory> findByClub_ClubId(Long clubId);
-}
--- src/main/java/woohakdong/server/domain/school/SchoolRepository.java
+++
@@ -1,9 +0,0 @@
-package woohakdong.server.domain.school;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface SchoolRepository extends JpaRepository<School, Long> {
-    Optional<School> findBySchoolDomain(String schoolDomain);
-}
