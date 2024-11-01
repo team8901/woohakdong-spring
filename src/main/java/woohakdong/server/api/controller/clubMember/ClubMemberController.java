eed5aa6f9c90da4739565db50b74b29cc61bb2db
sangjun <sangjun1389@ajou.ac.kr>
2024-11-02 02:14:31 +0900

    [WHD-222] Refactor: Unify endpoint formats

--- src/main/java/woohakdong/server/api/controller/clubMember/ClubMemberController.java
+++ src/main/java/woohakdong/server/api/controller/clubMember/ClubMemberController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/club")
+@RequestMapping("/v1/clubs")
 public class ClubMemberController implements ClubMemberControllerDocs{
 
     private final ClubMemberService clubMemberService;
