ef1113e0256526c5d264907174929bbb4e59d426
sangjun <sangjun1389@ajou.ac.kr>
2024-11-01 15:14:18 +0900

    [WHD-221] Refactor: Add exception layer to ClubMember repo

--- src/main/java/woohakdong/server/domain/clubmember/ClubMemberJpaRepository.java
+++ src/main/java/woohakdong/server/domain/clubmember/ClubMemberJpaRepository.java
@@ -1,24 +1,21 @@
 package woohakdong.server.domain.clubmember;
 
-import java.time.LocalDate;
-import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import woohakdong.server.domain.club.Club;
 import woohakdong.server.domain.member.Member;
 
-public interface ClubMemberRepository {
-    ClubMember save(ClubMember clubMember);
+import java.time.LocalDate;
+import java.util.List;
 
-    List<ClubMember> getAllByMember(Member member);
-
-    List<ClubMember> getAll();
+public interface ClubMemberJpaRepository extends JpaRepository<ClubMember, Long> {
 
     Boolean existsByClubAndMemberAndClubMemberRole(Club club, Member member, ClubMemberRole clubMemberRole);
 
     Boolean existsByClubAndMember(Club club, Member member);
 
-
-    List<ClubMember> getByClubIdAndAssignedTerm(Club club, LocalDate assignedTerm);
-
     List<ClubMember> findByClubAndClubMemberAssignedTerm(Club club, LocalDate assignedTerm);
+
+    List<ClubMember> findAllByMember(Member member);
 }
