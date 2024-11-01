e77d8af7adc3c29aae993d8b1e0194155dc7dd65
sangjun <sangjun1389@ajou.ac.kr>
2024-11-01 17:34:27 +0900

    [WHD-221] Refactor: Add exception layer to School repo

---
+++ src/main/java/woohakdong/server/domain/school/SchoolRepositoryImpl.java
@@ -0,0 +1,36 @@
+package woohakdong.server.domain.school;
+
+import static woohakdong.server.common.exception.CustomErrorInfo.INVALID_SCHOOL_DOMAIN;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import woohakdong.server.common.exception.CustomException;
+
+@RequiredArgsConstructor
+@Repository
+public class SchoolRepositoryImpl implements SchoolRepository{
+
+    private final SchoolJpaRepository schoolJpaRepository;
+
+    @Override
+    public School save(School school) {
+        return schoolJpaRepository.save(school);
+    }
+
+    @Override
+    public School getBySchoolDomain(String schoolDomain) {
+        return schoolJpaRepository.findBySchoolDomain(schoolDomain)
+                .orElseThrow(() -> new CustomException(INVALID_SCHOOL_DOMAIN));
+    }
+
+    @Override
+    public long count() {
+        return schoolJpaRepository.count();
+    }
+
+    @Override
+    public School getById(Long schoolId) {
+        return schoolJpaRepository.findById(schoolId)
+                .orElseThrow(() -> new CustomException(INVALID_SCHOOL_DOMAIN));
+    }
+}
