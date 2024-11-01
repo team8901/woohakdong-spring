1b59aa505f4eee2f3acd2e38db8aae15ba69cc58
sangjun <sangjun1389@ajou.ac.kr>
2024-11-02 03:23:16 +0900

    [WHD-130] Docs: Add Swagger docs for ClubMember

--- src/main/java/woohakdong/server/domain/clubmember/ClubMemberRole.java
+++ src/main/java/woohakdong/server/domain/clubmember/ClubMemberRole.java
@@ -6,8 +6,11 @@ import lombok.RequiredArgsConstructor;
 public enum ClubMemberRole {
 
     PRESIDENT("회장"),
+    VICEPRESIDENT("부회장"),
+    SECRETARY("총무"),
     OFFICER("임원"),
-    MEMBER("회원");
+    MEMBER("회원")
+    ;
 
     private final String role;
 }
