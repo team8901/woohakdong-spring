58908c662b8854f096c411f2282f7e0416e0e5c6
sangjun <sangjun1389@ajou.ac.kr>
2024-11-01 14:17:49 +0900

    [WHD-221] Refactor: Add exception layer to ClubAccoutHistory repo

---
+++ src/main/java/woohakdong/server/domain/clubAccountHistory/ClubAccountHistoryJpaRepository.java
@@ -0,0 +1,20 @@
+package woohakdong.server.domain.clubAccountHistory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import woohakdong.server.domain.clubAccount.ClubAccount;
+
+import java.util.List;
+
+public interface ClubAccountHistoryJpaRepository extends JpaRepository<ClubAccountHistory, Long> {
+
+    @Query("SELECT h FROM ClubAccountHistory h " +
+            "WHERE h.clubAccount = :clubAccount " +
+            "AND FUNCTION('YEAR', h.clubAccountHistoryTranDate) = :year " +
+            "AND FUNCTION('MONTH', h.clubAccountHistoryTranDate) = :month " +
+            "ORDER BY h.clubAccountHistoryTranDate DESC")
+    List<ClubAccountHistory> findMonthlyTransactions(@Param("clubAccount") ClubAccount clubAccount,
+                                                     @Param("year") int year,
+                                                     @Param("month") int month);
+}
