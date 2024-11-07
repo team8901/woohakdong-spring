fdabef37ac6dbdabad4db924394a408b3ba40d21
juy4844 <uiyeop@ajou.ac.kr>
2024-11-07 16:11:02 +0900

    [WHD-208] Fix: change code review feedback

--- src/main/java/woohakdong/server/api/controller/admin/auth/dto/AdminInfoUpdateRequest.java
+++ src/main/java/woohakdong/server/api/controller/admin/auth/dto/AdminInfoUpdateRequest.java
@@ -1,9 +1,9 @@
 package woohakdong.server.api.controller.admin.auth.dto;
 
 public record AdminInfoUpdateRequest(
-        String username,
-        String name,
-        String email,
-        String password
+        String memberLoginId,
+        String memberName,
+        String memberEmail,
+        String memberPassword
 ) {
 }
