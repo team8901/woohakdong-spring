ca819be7cccd80ceb8281a5f3c6b9501b5753935
sangjun <sangjun1389@ajou.ac.kr>
2024-11-08 02:47:28 +0900

    [WHD-228] Feat: Implement email notification service

--- src/main/java/woohakdong/server/api/service/email/EmailClientImpl.java
+++ src/main/java/woohakdong/server/api/service/email/EmailClientImpl.java
@@ -14,46 +14,135 @@ import woohakdong.server.common.exception.CustomException;
 @RequiredArgsConstructor
 public class EmailClientImpl implements EmailClient {
 
-    private final JavaMailSender mailSender;
     private final String CLUB_INVITE_EMAIL_SUBJECT = "%s 동아리 초대장입니다.";
+    private final String CLUB_INFO_CHANGED_EMAIL_SUBJECT = "%s 동아리 정보가 변경되었습니다.";
+    private final String SCHEDULE_NOTICE_EMAIL_SUBJECT = "%s 동아리에 새로운 일정이 등록되었습니다!";
+
+    private final JavaMailSender mailSender;
 
     @Override
-    public void sendEmailForGroupJoin(String receiverEmail, String clubName, String groupChatLink,
-                                      String clubPassword, String receiverName) {
+    public void sendEmailForClubJoin(String receiverName, String receiverEmail, String clubName,
+                                     String clubDescription, String groupChatLink, String groupChatPassword) {
+        String htmlText = createClubJoinHtml(clubName, clubDescription, receiverName, groupChatPassword, groupChatLink);
+        sendEmail(htmlText, receiverEmail, CLUB_INVITE_EMAIL_SUBJECT.formatted(clubName));
+    }
+
+    @Override
+    public void sendEmailForUpdateClubInfo(String receiverName, String receiverEmail, String clubName,
+                                           String clubDescription, String groupChatLink, String clubPassword) {
+        String htmlText = createClubUpdateHtml(clubName, clubDescription, receiverName, clubPassword, groupChatLink);
+        sendEmail(htmlText, receiverEmail, CLUB_INFO_CHANGED_EMAIL_SUBJECT.formatted(clubName));
+    }
+
+    @Override
+    public void sendEmailForSchedule(String receiverName, String receiverEmail, String clubName, String scheduleTitle,
+                                     String scheduleContent, String scheduleDate) {
+        String htmlText = createScheduleInfoHtml(clubName, receiverName, scheduleTitle, scheduleContent, scheduleDate);
+        sendEmail(htmlText, receiverEmail, SCHEDULE_NOTICE_EMAIL_SUBJECT.formatted(clubName));
+    }
+
+    private void sendEmail(String htmlText, String receiverEmail, String subject) {
         try {
-            String htmlText = """
-                    <html>
-                        <body style="font-family: Arial, sans-serif; color: #333; background-color: #ffffff;">
-                            <div style="background-color: #ffffff; padding: 20px; border-radius: 5px; max-width: 600px; margin: 0 auto; border: 1px solid #b3e0ff;">
-                                <h3 style="color: #3399ff;">%s 동아리 초대 링크입니다.</h3>
-                                <p>안녕하세요 %s님!. %s 동아리에 초대되셨습니다.</p>
-                                <p>아래의 링크를 클릭하여 동아리에서 잊지 못할 대학 생활을 만들어보세요!</p>
-                                <p>비밀번호: %s</p>
-                                <div style="text-align: center; margin: 20px 0;">
-                                    <a href="%s" style="display: inline-block; background-color: #3399ff; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px;">동아리 참여 링크</a>
-                                </div>
-                                <p style="text-align: right;">감사합니다.<br/>8901 드림</p>
-                            </div>
-
-                            <footer style="margin-top: 20px; text-align: center; font-size: 12px; color: #555;">
-                                <p>Contact us at: <a href="mailto:support@8901.dev@gmail.com" style="color: #3399ff;">8901.dev@gmail.com</a></p>
-                                <p>&copy; 2024 Woohakdong. All rights reserved.</p>
-                            </footer>
-
-                        </body>
-                    </html>
-                    """.formatted(clubName, receiverName, clubName, clubPassword, groupChatLink);
-
             MimeMessage message = mailSender.createMimeMessage();
             MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
             helper.setTo(receiverEmail);
-            helper.setSubject(CLUB_INVITE_EMAIL_SUBJECT.formatted(clubName));
+            helper.setSubject(subject);
             helper.setText(htmlText, true);
 
             mailSender.send(message);
         } catch (MessagingException e) {
             throw new CustomException(MAIL_SEND_ERROR);
         }
+    }
+
+    private String createClubJoinHtml(String clubName, String clubDescription, String receiverName, String clubPassword,
+                                      String groupChatLink) {
+        return """
+                <html>
+                           <body style="font-family: Arial, sans-serif; color: #333; background-color: #ffffff;">
+                               <div style="background-color: #ffffff; padding: 20px; border-radius: 5px; max-width: 600px; margin: 0 auto; border: 1px solid #b3e0ff;">
+                                   <h3 style="color: #3399ff;">%s 동아리 초대 링크입니다.</h3>
+
+                                   <p>안녕하세요 <strong>%s</strong>님. 새로운 동아리에 초대되었어요!</p>
+
+                                   <div style="background-color: #f7faff; padding: 15px; border-radius: 5px; margin-top: 10px;">
+                                       <p><strong>동아리 이름:</strong> %s</p>
+                                       <p><strong>동아리 소개:</strong> %s</p>
+                                       <p><strong>채팅방 비밀번호:</strong> %s</p>
+                                   </div>
+
+                                   <p>아래의 버튼을 클릭하여 동아리 채팅방에 접속해보세요!</p>
+
+                                   <div style="text-align: center; margin: 20px 0;">
+                                       <a href="%s" style="display: inline-block; background-color: #3399ff; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px;">동아리 채팅방 참여하기</a>
+                                   </div>
+                               </div>
+
+                               <footer style="margin-top: 20px; text-align: center; font-size: 12px; color: #555;">
+                                   <p>Contact us at: <a href="mailto:support@8901.dev@gmail.com" style="color: #3399ff;">8901.dev@gmail.com</a></p>
+                                   <p>&copy; 2024 Woohakdong. All rights reserved.</p>
+                               </footer>
+                           </body>
+                       </html>
+                """.formatted(clubName, receiverName, clubName, clubDescription, clubPassword, groupChatLink);
+    }
+
+    private String createClubUpdateHtml(String clubName, String clubDescription, String receiverName,
+                                        String clubPassword, String groupChatLink) {
+        return """
+                <html>
+                    <body style="font-family: Arial, sans-serif; color: #333; background-color: #ffffff;">
+                        <div style="background-color: #ffffff; padding: 20px; border-radius: 5px; max-width: 600px; margin: 0 auto; border: 1px solid #b3e0ff;">
+                            <h3 style="color: #3399ff;">%s 동아리의 정보 변경사항입니다.</h3>
+
+                            <p>안녕하세요 <strong>%s</strong>님. 동아리의 변경된 정보는 다음과 같아요!</p>
+
+                                <div style="background-color: #f7faff; padding: 15px; border-radius: 5px; margin-top: 10px;">
+                                    <p><strong>동아리 이름:</strong> %s</p>
+                                    <p><strong>동아리 소개:</strong> %s</p>
+                                    <p><strong>채팅방 비밀번호:</strong> %s</p>
+                                </div>
+
+                            <p>아래의 버튼을 클릭하여 변경된 동아리 채팅방에 접속해보세요!</p>
+
+                            <div style="text-align: center; margin: 20px 0;">
+                                <a href="%s" style="display: inline-block; background-color: #3399ff; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px;">동아리 채팅방</a>
+                            </div>
+                        </div>
+
+                        <footer style="margin-top: 20px; text-align: center; font-size: 12px; color: #555;">
+                            <p>Contact us at: <a href="mailto:support@8901.dev@gmail.com" style="color: #3399ff;">8901.dev@gmail.com</a></p>
+                            <p>&copy; 2024 Woohakdong. All rights reserved.</p>
+                        </footer>
+                    </body>
+                </html>
+                """.formatted(clubName, receiverName, clubName, clubDescription, clubPassword, groupChatLink);
+    }
+
+    private String createScheduleInfoHtml(String clubName, String receiverName, String scheduleTitle,
+                                          String scheduleContent, String scheduleDateTime) {
+        return """
+                <html>
+                    <body style="font-family: Arial, sans-serif; color: #333; background-color: #ffffff;">
+                        <div style="background-color: #ffffff; padding: 20px; border-radius: 5px; max-width: 600px; margin: 0 auto; border: 1px solid #b3e0ff;">
+                            <h3 style="color: #3399ff;">%s 동아리에 새로운 일정이 등록되었습니다!</h3>
+
+                            <p>안녕하세요 <strong>%s</strong>님. %s 동아리에 새로운 일정이 추가되었어요!</p>
+
+                            <div style="background-color: #f7faff; padding: 15px; border-radius: 5px; margin-top: 10px;">
+                                <p><strong>일정 제목:</strong> %s</p>
+                                <p><strong>일정 내용:</strong> %s</p>
+                                <p><strong>일정 시간:</strong> %s</p>
+                            </div>
+                        </div>
+
+                        <footer style="margin-top: 20px; text-align: center; font-size: 12px; color: #555;">
+                            <p>Contact us at: <a href="mailto:support@8901.dev@gmail.com" style="color: #3399ff;">8901.dev@gmail.com</a></p>
+                            <p>&copy; 2024 Woohakdong. All rights reserved.</p>
+                        </footer>
+                    </body>
+                </html>
+                """.formatted(clubName, receiverName, clubName, scheduleTitle, scheduleContent, scheduleDateTime);
     }
 }
---
+++ src/test/java/woohakdong/server/api/service/notification/NotificationServiceTest.java
@@ -0,0 +1,162 @@
+package woohakdong.server.api.service.notification;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.willDoNothing;
+import static woohakdong.server.domain.clubmember.ClubMemberRole.OFFICER;
+import static woohakdong.server.domain.member.MemberGender.MAN;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.SyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+import woohakdong.server.api.service.email.EmailClientImpl;
+import woohakdong.server.common.security.jwt.CustomUserDetails;
+import woohakdong.server.domain.club.Club;
+import woohakdong.server.domain.club.ClubRepository;
+import woohakdong.server.domain.clubmember.ClubMember;
+import woohakdong.server.domain.clubmember.ClubMemberRepository;
+import woohakdong.server.domain.member.Member;
+import woohakdong.server.domain.member.MemberRepository;
+import woohakdong.server.domain.schedule.Schedule;
+import woohakdong.server.domain.schedule.ScheduleRepository;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+class NotificationServiceTest {
+
+    @Autowired
+    private NotificationService notificationService;
+
+    @MockBean
+    private EmailClientImpl emailClientImpl;
+
+    @TestConfiguration
+    static class TestConfig {
+
+        @Bean
+        public TaskExecutor taskExecutor() {
+            return new SyncTaskExecutor();
+        }
+    }
+
+    @Autowired
+    private ClubMemberRepository clubMemberRepository;
+
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+
+    @Autowired
+    private ClubRepository clubRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    void setUp() {
+        String provideId = "testProvideId";
+        CustomUserDetails userDetails = new CustomUserDetails(provideId, "USER_ROLE");
+        Authentication auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        member1 = createMember(provideId, "박상준", "sangjun@example.com");
+        Member member2 = createMember("testProvideId2", "김우학", "woohak@example.com");
+        Member member3 = createMember("testProvideId3", "최햇반", "hatban@example.com");
+        club = createClub("두잇");
+        createClubMember(member1, club);
+        createClubMember(member2, club);
+        createClubMember(member3, club);
+    }
+
+    private Member member1;
+    private Club club;
+
+    @DisplayName("동아리 정보를 포함한 이메일을 동아리원들에게 전송할 수 있다.")
+    @Test
+    void sendNotificationWithClubInfoUpdate() {
+        // Given
+        Long clubId = club.getClubId();
+        LocalDate assignedTerm = LocalDate.of(2024, 7, 1);
+
+        willDoNothing()
+                .given(emailClientImpl)
+                .sendEmailForUpdateClubInfo(any(), any(), any(), any(), any(), any());
+
+        // When & Then
+        notificationService.sendNotificationWithClubInfoUpdate(clubId, assignedTerm);
+    }
+
+    @DisplayName("일정 정보를 포함한 이메일을 동아리원들에게 전송할 수 있다.")
+    @Test
+    void sendNotificationWithSchedule() {
+        // Given
+        Schedule schedule = createSchedule("일정 제목", "일정 내용", LocalDateTime.of(2024, 11, 7, 12, 0));
+
+        Long clubId = club.getClubId();
+        Long scheduleId = schedule.getScheduleId();
+        LocalDate assignedTerm = LocalDate.of(2024, 7, 1);
+
+        willDoNothing()
+                .given(emailClientImpl)
+                .sendEmailForSchedule(any(), any(), any(), any(), any(), any());
+
+        // When & Then
+        notificationService.sendNotificationWithSchedule(clubId, scheduleId, assignedTerm);
+    }
+
+    private Member createMember(String provideId, String name, String email) {
+        Member member = Member.builder()
+                .memberProvideId(provideId)
+                .memberName(name)
+                .memberEmail(email)
+                .memberPhoneNumber("01012345678")
+                .memberMajor("Computer Science")
+                .memberStudentNumber("20210001")
+                .memberGender(MAN)
+                .build();
+        return memberRepository.save(member);
+    }
+
+    private Club createClub(String name) {
+        Club club = Club.builder()
+                .clubName(name)
+                .clubEnglishName("testClub")
+                .clubDescription("testDescription")
+                .clubGroupChatLink("https://club-group-chat-link.com")
+                .build();
+        return clubRepository.save(club);
+    }
+
+    private ClubMember createClubMember(Member member, Club club) {
+        ClubMember clubMember = ClubMember.builder()
+                .member(member)
+                .club(club)
+                .clubMemberAssignedTerm(LocalDate.of(2024, 7, 1))
+                .clubMemberRole(OFFICER)
+                .clubJoinedDate(LocalDate.of(2024, 8, 1))
+                .build();
+        return clubMemberRepository.save(clubMember);
+    }
+
+    private Schedule createSchedule(String title, String content, LocalDateTime date) {
+        Schedule schedule = Schedule.builder()
+                .scheduleTitle(title)
+                .scheduleContent(content)
+                .scheduleDateTime(date)
+                .club(club)
+                .build();
+        return scheduleRepository.save(schedule);
+    }
+}
