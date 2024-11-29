fdc99bad6fafd44f1c690b1ccae103a3ee3f68e2
juy4844 <uiyeop@ajou.ac.kr>
2024-11-30 01:36:55 +0900

    [WHD-274] Feat: Inquiry, Item category explanation english to korean

--- src/main/java/woohakdong/server/domain/item/ItemCategory.java
+++ src/main/java/woohakdong/server/domain/item/ItemCategory.java
@@ -5,12 +5,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ItemCategory {
 
-    BOOK("book"),
-    SPORT("sports"),
-    DIGITAL("digital"),
-    CLOTHES("clothes"),
-    STATIONERY("stationery"),
-    ETC("etc");
+    BOOK("책"),
+    SPORT("스포츠"),
+    DIGITAL("디지털"),
+    CLOTHES("의류"),
+    STATIONERY("문구류"),
+    ETC("기타");
 
     private final String category;
 }
