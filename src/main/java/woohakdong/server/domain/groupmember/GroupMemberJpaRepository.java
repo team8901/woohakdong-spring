70185ab861e334ed1ec20a906a7fd886363452a9
sangjun <sangjun1389@ajou.ac.kr>
2024-12-02 20:55:42 +0900

    [WHD-255] Fix: Prevent duplicate group join

--- src/main/java/woohakdong/server/domain/groupmember/GroupMemberJpaRepository.java
+++ src/main/java/woohakdong/server/domain/groupmember/GroupMemberJpaRepository.java
@@ -2,8 +2,12 @@ package woohakdong.server.domain.groupmember;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import woohakdong.server.domain.clubmember.ClubMember;
 import woohakdong.server.domain.group.Group;
+import woohakdong.server.domain.member.Member;
 
 public interface GroupMemberJpaRepository extends JpaRepository<GroupMember, Long> {
     List<GroupMember> findAllByGroup(Group group);
+
+    boolean existsByGroupAndClubMember(Group group, ClubMember clubMember);
 }
--- src/main/java/woohakdong/server/domain/groupmember/GroupMemberRepositoryImpl.java
+++ src/main/java/woohakdong/server/domain/groupmember/GroupMemberRepositoryImpl.java
@@ -3,7 +3,9 @@ package woohakdong.server.domain.groupmember;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import woohakdong.server.domain.clubmember.ClubMember;
 import woohakdong.server.domain.group.Group;
+import woohakdong.server.domain.member.Member;
 
 @RequiredArgsConstructor
 @Repository
@@ -12,7 +14,17 @@ public class GroupMemberRepositoryImpl implements GroupMemberRepository {
     private final GroupMemberJpaRepository groupMemberJpaRepository;
 
     @Override
+    public GroupMember save(GroupMember groupMember) {
+        return groupMemberJpaRepository.save(groupMember);
+    }
+
+    @Override
     public List<GroupMember> getByGroup(Group group) {
         return groupMemberJpaRepository.findAllByGroup(group);
+    }
+
+    @Override
+    public boolean checkAlreadyJoined(Group group, ClubMember clubMember) {
+        return groupMemberJpaRepository.existsByGroupAndClubMember(group, clubMember);
     }
 }
